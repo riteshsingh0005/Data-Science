4c3c0b6b26516e0b698a60e61c7275637a7451ae
Ritesh Singh <mdxritesh1234@gmail.com>
2025-06-27 18:15:33 +0530

    first commit

---
+++ armstrong.py
@@ -0,0 +1,14 @@
+#armstrong.py
+num = int(input("Enter a number: "))
+original = num
+sum = 0
+
+while num > 0:
+    digit = num%10
+    sum = sum + (digit ** 3)
+    num = num // 10
+
+if original == sum:
+    print(f"{original} is an Armstrong number")
+else:
+    print(f"{original} is not an Armstrong number")
